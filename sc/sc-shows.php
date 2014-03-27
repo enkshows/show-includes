fc04076700013f2706f89b063d8057212fb01d31
CVillazor <cvillazor@yahoo.com>
2014-03-27 12:05:22 -0400

    Update SC Feb and Sept 2014 Show Date/Hour

--- sc/sc-shows.php
+++ sc/sc-shows.php
@@ -2,11 +2,10 @@
   <h2>Sole Commerce - 2014 Show Dates</h2>
   <div class="desc-lg">
    <span class="info-2013-shows">
-     <a href="http://www.dev.enkshows.com/sole">
-      <strong>February 23.24.25, 2014 @ Javits Center.NYC</strong></a><br>
+     <a href="http://www.enkshows.com/sole/2014/feb/"><strong>February 23.24.25, 2014 @ Javits Center.NYC</strong></a><br>
       Sunday/Monday, 9am - 6pm  • Tuesday, 9am - 5pm <br><br>
-
-      <strong>September TBD, 2014 @ Javits Center.NYC</strong><br>
+    
+     <a href="http://www.enkshows.com/sole/2014/feb/"><strong>September, 2014 @ Javits Center.NYC</strong></a><br>
     </span>
   </div>
 </div>
